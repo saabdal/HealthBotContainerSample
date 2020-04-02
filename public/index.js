07091f04c0ffda087abd2b472291ecbc0c287454
Sami Abdallah <45918880+saabdal@users.noreply.github.com>
2020-04-02 10:21:27 -0400

    Update index.js

--- public/index.js
+++ public/index.js
@@ -107,15 +107,11 @@ function initBotConversation() {
                             jsonWebToken: jsonWebToken,
 
                             // Use the following activity to proactively invoke a bot scenario
-                            /*
+                            
                             triggeredScenario: {
-                                trigger: "{scenario_id}",
-                                args: {
-                                    myVar1: "{custom_arg_1}",
-                                    myVar2: "{custom_arg_2}"
-                                }
+                                trigger: "covid19_assessment"
                             }
-                            */
+                            
                         }
                     }
                 }
